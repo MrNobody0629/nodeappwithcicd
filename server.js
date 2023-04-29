042ffb34eeb2fe18fc026f5448206e9696a5dbdc
Anuj Verma <109571920+anuj-antino@users.noreply.github.com>
2023-04-29 17:36:03 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require("express");
+require("dotenv").config();
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => res.send("Hello from NODE WITH CI/CD!"));
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}!`);
+});
